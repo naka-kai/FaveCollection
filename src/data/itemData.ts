f746c8ed30b944978ac0af8ddc7ed704fd12e461
kai <sea.k.naka@gmail.com>
2024-08-30 23:31:16 +0900

    コレクション詳細のレイアウト作成・データ作成

--- src/data/itemData.ts
+++ src/data/itemData.ts
@@ -11,63 +11,152 @@ import aloha_vertical_1 from "/public/img/aloha_vertical_1.jpg";
 import aloha_vertical_2 from "/public/img/aloha_vertical_2.jpg";
 import aloha_vertical_3 from "/public/img/aloha_vertical_3.jpg";
 
-export const itemData = [
+type CategoryType = {
+  num: number;
+  name: string;
+  color: string;
+};
+
+const category: { [key: number]: CategoryType } = {
+  2: { num: 2, name: "小笠原海", color: "#3301FF" },
+  3: { num: 3, name: "船津稜雅", color: "#CA32FF" },
+  4: { num: 4, name: "草川拓弥", color: "#00CC34" },
+  5: { num: 5, name: "村田祐基", color: "#FE0202" },
+  7: { num: 7, name: "松尾太陽", color: "#CCCCCC" },
+  11: { num: 11, name: "志村秀哉", color: "#7C7C7C" },
+  12: { num: 12, name: "森次政裕", color: "#935A04" },
+  13: { num: 13, name: "髙松アロハ", color: "#11E0D9" },
+  14: { num: 14, name: "柏木悠", color: "#FC8A07" },
+};
+
+type itemDataType = {
+  img: string;
+  title: string;
+  category: CategoryType;
+  tag: string;
+  tourOpeningDate: Date;
+  releaseDate: Date;
+  num: number;
+  rows?: number;
+  cols?: number;
+};
+
+export const itemData: itemDataType[] = [
   {
     img: `${aloha_square_1}`,
-    title: "aloha_square_1",
+    title: "生写真セットA 右から1番目",
+    category: category[13],
+    tag: "#Rail_is_Beautiful",
+    tourOpeningDate: new Date(2024, 10, 1),
+    releaseDate: new Date(2024, 10, 1),
+    num: 1,
     rows: 2,
     cols: 2,
-    tag: "#Rail is Beautiful",
   },
   {
     img: `${aloha_horizontal_1}`,
-    title: "aloha_horizontal_1",
+    title: "生写真セットC 右から2番目",
+    category: category[13],
+    tag: "#Rail_is_Beautiful",
+    tourOpeningDate: new Date(2024, 10, 1),
+    releaseDate: new Date(2024, 10, 1),
+    num: 1,
   },
   {
     img: `${aloha_horizontal_2}`,
-    title: "aloha_horizontal_2",
+    title: "生写真セットA 右から3番目",
+    category: category[13],
+    tag: "#Rail_is_Beautiful",
+    tourOpeningDate: new Date(2024, 10, 1),
+    releaseDate: new Date(2024, 10, 1),
+    num: 1,
   },
   {
     img: `${aloha_horizontal_3}`,
-    title: "aloha_horizontal_3",
+    title: "生写真セットB 右から2番目",
+    category: category[13],
+    tag: "#T.I.M.E",
+    tourOpeningDate: new Date(2024, 10, 1),
+    releaseDate: new Date(2024, 10, 1),
+    num: 1,
     cols: 2,
   },
   {
     img: `${aloha_horizontal_4}`,
-    title: "aloha_horizontal_4",
+    title: "生写真セットA 右から5番目",
+    category: category[13],
+    tag: "#Rail_is_Beautiful",
+    tourOpeningDate: new Date(2024, 10, 1),
+    releaseDate: new Date(2024, 10, 1),
+    num: 1,
     cols: 2,
   },
   {
     img: `${aloha_square_2}`,
-    title: "aloha_square_2",
+    title: "生写真セットB 右から1番目",
+    category: category[13],
+    tag: "#Rail_is_Beautiful",
+    tourOpeningDate: new Date(2024, 10, 1),
+    releaseDate: new Date(2024, 10, 1),
+    num: 2,
     rows: 2,
     cols: 2,
   },
   {
     img: `${aloha_square_3}`,
-    title: "aloha_square_3",
+    title: "生写真セットA 右から2番目",
+    category: category[13],
+    tag: "#T.I.M.E",
+    tourOpeningDate: new Date(2024, 10, 1),
+    releaseDate: new Date(2024, 10, 1),
+    num: 1,
   },
   {
     img: `${aloha_square_4}`,
-    title: "aloha_square_4",
+    title: "生写真セットB 右から3番目",
+    category: category[13],
+    tag: "#Rail_is_Beautiful",
+    tourOpeningDate: new Date(2024, 10, 1),
+    releaseDate: new Date(2024, 10, 1),
+    num: 1,
   },
   {
     img: `${aloha_vertical_1}`,
-    title: "aloha_vertical_1",
+    title: "生写真セットA 右から4番目",
+    category: category[13],
+    tag: "#Rail_is_Beautiful",
+    tourOpeningDate: new Date(2024, 10, 1),
+    releaseDate: new Date(2024, 10, 1),
+    num: 3,
     rows: 2,
     cols: 2,
   },
   {
     img: `${aloha_vertical_2}`,
-    title: "aloha_vertical_2",
+    title: "生写真セットB 右から5番目",
+    category: category[13],
+    tag: "#T.I.M.E",
+    tourOpeningDate: new Date(2024, 10, 1),
+    releaseDate: new Date(2024, 10, 1),
+    num: 1,
   },
   {
     img: `${aloha_vertical_3}`,
-    title: "aloha_vertical_3",
+    title: "生写真セットC 右から1番目",
+    category: category[13],
+    tag: "#Rail_is_Beautiful",
+    tourOpeningDate: new Date(2024, 10, 1),
+    releaseDate: new Date(2024, 10, 1),
+    num: 2,
   },
   {
     img: `${aloha_square_5}`,
-    title: "aloha_square_5",
+    title: "生写真セットB 右から4番目",
+    category: category[13],
+    tag: "#T.I.M.E",
+    tourOpeningDate: new Date(2024, 10, 1),
+    releaseDate: new Date(2024, 10, 1),
+    num: 1,
     cols: 2,
   },
 ];
--- src/pages/Collection/Show.tsx
+++ src/pages/Collection/Show.tsx
@@ -1,7 +1,65 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { itemData } from "../../data/itemData";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import RemoveCircleIcon from "@mui/icons-material/RemoveCircle";
 
 const Show = () => {
-  return <div>Show</div>;
+  const [count, setCount] = useState<number>(0);
+
+  useEffect(() => {
+    if (item) {
+      setCount(item.num);
+    }
+  }, []);
+
+  const { id } = useParams();
+  if (!id) {
+    return (
+      <div className="mt-[70px] h-screen flex items-center justify-center">
+        <p className="text-3xl">IDが正しくありません</p>
+      </div>
+    );
+  }
+
+  const item = itemData.find((item, index) => index + 1 === parseInt(id));
+  if (!item) {
+    return (
+      <div className="mt-[70px] h-screen flex items-center justify-center">
+        <p className="text-3xl">アイテムが見つかりません</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="my-[70px] w-full flex justify-center">
+      <div className="w-2/3">
+        <h2 className="text-4xl text-center mb-5">{item.title}</h2>
+        <figure>
+          <img src={item.img} alt={item.title} />
+        </figure>
+        <div className="my-2 flex items-center">
+          <div
+            className={`bg-[${item.category.color}] mr-3 py-1 px-3 rounded-sm text-white`}
+          >
+            <span className="text-xl">{item.category.name}</span>
+          </div>
+          <span className="text-lg text-gray-400">{item.tag}</span>
+        </div>
+        <div className="flex justify-end items-center">
+          <button
+            onClick={() => setCount((prev) => (prev - 1 >= 0 ? prev - 1 : 0))}
+          >
+            <RemoveCircleIcon className="text-pink-300" fontSize="large" />
+          </button>
+          <p className="text-4xl mx-2">{count}</p>
+          <button onClick={() => setCount((prev) => prev + 1)}>
+            <AddCircleIcon className="text-pink-300" fontSize="large" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Show;
